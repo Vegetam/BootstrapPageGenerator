e3d699c3dfd223a8788d14c14c2e206fcef88783
Francesco Malagrino <francesco.malagrino@gmail.com>
2014-02-24 19:54:05 +0100

    Version 1.1
    Version 1.1

--- js/scripts.js
+++ js/scripts.js
@@ -470,6 +470,7 @@ $(document).ready(function() {
 
 function saveHtml() 
 			{
+			webpage = '<html>\n<head>\n<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-2.0.0.min.js"></script>\n<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-ui"></script>\n<link href="http://www.francescomalagrino.com/BootstrapPageGenerator/3/css/bootstrap-combined.min.css" rel="stylesheet" media="screen">\n<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/bootstrap.min.js"></script>\n</head>\n<body>\n'+ webpage +'\n</body>\n</html>'
 			/* FM aka Vegetam Added the function that save the file in the directory Downloads. Work only to Chrome Firefox And IE*/
 			if (navigator.appName =="Microsoft Internet Explorer" && window.ActiveXObject)
 			{
@@ -487,13 +488,13 @@ function saveHtml()
 			write(webpage);
 			close();
 			document.charset = "utf-8";
-			dlg = execCommand('SaveAs', false, locationFile);
-			body.removeChild(ir);
+			dlg = execCommand('SaveAs', false, locationFile+"webpage.html");
 			}
     return dlg;
 			}
 			}
 			else{
+			webpage = webpage;
 			var blob = new Blob([webpage], {type: "text/html;charset=utf-8"});
 			saveAs(blob, "webpage.html");
 		}
