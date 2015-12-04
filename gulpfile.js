2e75d1c7af8f1f0cb4058845f100548a46eb8fc2
Angel Ordax <angel.ordax@devex.com>
2015-12-04 17:40:33 +0100

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var        gulp = require('gulp'),
 var        sass = require('gulp-sass');
 
 
-
 gulp.task('sass', function () {
   gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
