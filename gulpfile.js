9730b63b1fa6516661fd9fdb4a3f718250023f62
Jesus Herman <jesus.herman@devex.com>
2015-12-04 16:39:20 +0100

    sidebar content padding & compiling issue

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('scss/*.scss')
+  gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
